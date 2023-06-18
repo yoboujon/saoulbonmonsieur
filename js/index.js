023b9606b5f12d16888b5fec52839c2bad405474
Yohan Boujon <yo.boujon@gmail.com>
2023-06-18 23:54:21 +0200

    Fixed Animation and presentation class

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
 var saoulAnimation = anime({
     targets: '.photosaoul',
-    autoplay: false,
-    rotate: -30,
-    translateX: -380,
+    easing: 'easeOutQuint',
+    translateX: [
+        {value: 0, duration: 100, delay: 0 },
+        {value: "100%", duration: 600, delay: 100 }
+    ],
+    rotate : [
+        {value: 0, duration: 100, delay: 0 },
+        {value: 30, duration: 600, delay: 100 }
+    ],
+    begin: function () {
+        document.getElementsByClassName("photosaoul")[0].style.display = "flex";
+    },
 });
 
+saoulAnimation.reverse()
+
 function superAnim() {
-    saoulAnimation.restart();
+    saoulAnimation.restart()
 }
