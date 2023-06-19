16294ee25f37604b39256f64b60d398e1bbc1b27
Yohan Boujon <yo.boujon@gmail.com>
2023-06-19 02:07:08 +0200

    Added text animation and a background image

--- js/componentloader.js
+++ js/componentloader.js
@@ -14,3 +14,14 @@ function load(url, element) {
 window.addEventListener('load', function () {
     load("../components/navbar.html", document.getElementsByTagName("Navbar")[0]);
 })
+
+function ready(callback){
+    // in case the document is already rendered
+    if (document.readyState!='loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function(){
+        if (document.readyState=='complete') callback();
+    });
+}
--- js/index.js
+++ js/index.js
@@ -2,20 +2,46 @@ var saoulAnimation = anime({
     targets: '.photosaoul',
     easing: 'easeOutQuint',
     translateX: [
-        {value: 0, duration: 100, delay: 0 },
-        {value: "100%", duration: 600, delay: 100 }
+        {value: "", duration: 100, delay: 0 },
+        {value: "130%", duration: 1000, delay: 100 }
     ],
     rotate : [
         {value: 0, duration: 100, delay: 0 },
-        {value: 30, duration: 600, delay: 100 }
+        {value: -60, duration: 1000, delay: 100 }
     ],
     begin: function () {
         document.getElementsByClassName("photosaoul")[0].style.display = "flex";
     },
 });
 
+var whiteBandAnimation = anime({
+    targets: '.whiteband',
+    easing: 'easeInExpo',
+    width: "100%",
+    duration: 2200,
+})
+
+var textAnimation = anime({
+    targets: '.anim-text',
+    easing: 'easeInOutExpo',
+    opacity : [
+        {value: 0, duration: 2000, delay: 0 },
+        {value: 100, duration: 1000, delay: 100 }
+    ],
+    begin: function () {
+        document.getElementsByClassName("anim-text")[0].style.display = "block";
+    },
+})
+
 saoulAnimation.reverse()
+ready(function(){
+    setTimeout(() => {
+        saoulAnimation.play();
+        whiteBandAnimation.play();
+        textAnimation.play();
+    }, 1000);
+});
 
 function superAnim() {
-    saoulAnimation.restart()
+    saoulAnimation.play()
 }
